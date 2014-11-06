31faa59cb4a382738bf1c93c94ba0f7075700dae
Ethan Brown <ethan.w.brown@gmail.com>
2014-11-06 18:49:40 +0100

    added Makefile for stats.so

--- scripts/Stats.py
+++ scripts/Stats.py
@@ -3,13 +3,9 @@ import os
 import ctypes as ct
 import numpy as np
 
-def we_are_frozen():
-    # All of the modules are built-in to the interpreter, e.g., by py2exe
-    return hasattr(sys, "frozen")
-
 def module_path():
     encoding = sys.getfilesystemencoding()
-    if we_are_frozen():
+    if hasattr(sys, "frozen"):
         return os.path.dirname(unicode(sys.executable, encoding))
     return os.path.dirname(unicode(__file__, encoding))
 
