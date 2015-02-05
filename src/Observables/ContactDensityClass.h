d92d7d834ba8bf2eb5c41a3f537ef446d785f057
Ethan Brown <ethan.w.brown@gmail.com>
2015-02-05 17:29:48 +0100

    forgot to add moved files

---
+++ src/Observables/ContactDensityClass.h
@@ -0,0 +1,31 @@
+#ifndef ContactDensityClass_H
+#define ContactDensityClass_H
+
+#include "ObservableClass.h"
+#include "../Actions/ActionClass.h"
+
+class ContactDensity : public Observable
+{
+private:
+  std::vector< std::shared_ptr<Action> > actionList, &fullActionList;
+  string speciesA, speciesB;
+  int iSpeciesA, iSpeciesB;
+  int ZA;
+  double total;
+protected:
+public:
+  ContactDensity(Path &tmpPath, std::vector< std::shared_ptr<Action> > &tmpActionList, Input &in, IOClass &out)
+    : fullActionList(tmpActionList), Observable(tmpPath, in, out)
+  {
+    Init(in);
+    string data_type = "scalar";
+    out.Write(prefix+"/data_type",data_type);
+  }
+
+  virtual void Init(Input &in);
+  virtual void Reset();
+  virtual void Accumulate();
+  virtual void Write();
+};
+
+#endif
