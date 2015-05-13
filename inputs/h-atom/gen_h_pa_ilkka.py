2e2141e881bb5639a77be46c87e63f1b7af74ec3
Ethan Brown <ethan.w.brown@gmail.com>
2015-05-13 21:21:18 +0200

    updated pagen and associated pair action generating scripts

--- inputs/h-atom/gen_h_pa_ilkka.py
+++ inputs/h-atom/gen_h_pa_ilkka.py
@@ -1,38 +1,22 @@
 import sys, os
-from math import sqrt, pi
+from math import sqrt
 
 # Exact location of PAGEN scripts
-PAGEN_HOME = '../../scripts/pagen'
+PAGEN_HOME = os.environ['HOME']+'/src/simpimc/scripts/pagen'
 sys.path.append(PAGEN_HOME)
 from GenPairAction import *
 
 # Units
 units = {'energy':'H', 'distance':'A'}
 
-# Settings for e-gas
-M = 128 # number of time slices
-N = 7 # number electrons
-pol = 1 # if polarized or not
-theta = 1. # T/T_F
-rs = 1. # Wigner Seits Radius
+# Constants
+tau = 0.125 # Time step
+L = 100.0 # Box size
+D = 3 # physical dimension
 
-# Calculate things
-# Fermi Temp (see Martin pg 103 (only he uses atomic units))
-if pol:
-  TF = 0.5 * (9.*pi/2.)**(2./3.) / (rs**2)
-else:
-  TF = 0.5 * (9.*pi/4.)**(2./3.) / (rs**2)
-T = theta*TF # temperature in Hartree
-tau = (1/T)/M # used tau
-a0 = 1. # Bohr radius (.52917721092 Angstroms)
-a = rs*a0
-from math import pi, sqrt
-L = pow(N*(4./3.)*pi*a*a*a, 1.0/3.0) # Length of box
-k_cut = 14./(L/2.)
-print 'tau', tau, 'k_cut', k_cut, 'L', L
-
-# Particles
+# Species
 e = {'type': 'e', 'lambda': 0.5, 'Z': -1.0}
+p = {'type': 'p', 'lambda': 0.0002723089072243553, 'Z': 1.0}
 
 # Potential
 potential = {}
@@ -44,9 +28,9 @@ potential['grid_type'] = "OPTIMIZED" # grid type (LINEAR, LOG, OPTIMIZED (Ilkka 
 
 # Squarer
 squarer = {}
-squarer['type'] = "Ilkka" # Ilkka or David
+squarer['type'] = "Ilkka" # Ilkka, David, or None
 squarer['tau'] = tau # desired timestep of PIMC simulation
-squarer['n_d'] = 3 # dimension
+squarer['n_d'] = D # dimension
 squarer['r_max'] = 100.0 # maximum distance on grid
 squarer['n_grid'] = 100 # number of grid points
 squarer['grid_type'] = "OPTIMIZED" # grid type (LINEAR, LOG, OPTIMIZED (Ilkka only!))
@@ -56,13 +40,14 @@ squarer['n_temp'] = 1 # number of temperatures for which to calculate the pair a
 
 # Long-range breakup
 breakup = {}
-breakup['type'] = 'OptimizedEwald' # OptimizedEwald, StandardEwald, or None
-breakup['n_d'] = 3 # dimension
+breakup['type'] = 'None' # OptimizedEwald, StandardEwald, or None
+breakup['n_d'] = D # dimension
 breakup['L'] = L # length of box
+breakup['tau'] = tau # desired timestep of PIMC simulation
 breakup['r_min'] = 0.0001 # first grid point
 breakup['r_max'] = sqrt(breakup['n_d'])*breakup['L']/2. # last grid point
 breakup['r_cut'] = breakup['L']/2. # r cutoff for ewald
-breakup['k_cut'] = k_cut # k cutoff for ewald
+breakup['k_cut'] = 14./(L/2.) # k cutoff for ewald
 breakup['n_grid'] = 1000 # number of grid points
 breakup['grid_type'] = "OPTIMIZED" # grid type (LINEAR, LOG, OPTIMIZED (Ilkka only!))
 breakup['n_knots'] = 10 # number of knots in spline (probably fine)
@@ -70,7 +55,7 @@ breakup['n_images'] = 10 # Naive check
 
 # Pair action objects
 pa_objects = [
-{'species_a': e, 'species_b': e, 'potential': potential, 'breakup': breakup, 'squarer': squarer},
+{'species_a': e, 'species_b': p, 'potential': potential, 'breakup': breakup, 'squarer': squarer},
 ]
 
 # Run
