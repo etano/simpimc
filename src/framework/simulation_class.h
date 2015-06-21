3393b324a49d8624e8b2aff2fb77f5735e0e533c
Ethan Brown <ethan.w.brown@gmail.com>
2015-06-21 20:00:31 +0200

    reorganized framework slightly

--- src/framework/simulation_class.h
+++ src/framework/simulation_class.h
@@ -1,112 +1,47 @@
 #ifndef SIMPIMC_SIMULATION_CLASS_H_
 #define SIMPIMC_SIMULATION_CLASS_H_
 
-#include <vector>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <assert.h>
-#include <memory>
-#include <sys/time.h>
-#include <atomic>
+#include "../actions/actions.h"
+#include "../events/events.h"
 
-#include <scaffold/matrix/matrix.h>
-#include <scaffold/algorithm/algorithm.h>
-#include <scaffold/communication/communication.h>
-#include <scaffold/io/io_xml.h>
-#include <scaffold/io/io_hdf5.h>
-#include <scaffold/rng/rng.h>
-
-using namespace scaffold::matrix;
-using namespace scaffold::algorithm;
-using namespace scaffold::io;
-using namespace scaffold::parallel;
-using namespace scaffold::rand;
-
-#include "algorithm_class.h"
-
-/// Class that handles parallelization. It contains the instantiation of the algorithm and random number generator
+/// Class the actually holds all the events and data objects. This includes the path object which is the main container of all relevant information to the path.
 class Simulation
 {
 private:
-  Input in; ///< Input file object
-  IO out; ///< Output file object
-
-  Communicator world_comm; ///< This is the global MPIWORLD communicator.
-  Communicator inter_comm; ///< This is for communication between the rank 0 procs of each walker group.
-  Communicator intra_comm; ///< This is for commmunication between procs within a walker group.
+  Loop main_loop; ///< Main loop, contains algorithm
+  std::vector<std::shared_ptr<Event>> events; /// < Vector of events
+  std::vector<std::shared_ptr<Action>> actions; ///< Vector of actions
+  Path path; ///< Main data structure
 public:
-  /// Constructor does nothing.
-  Simulation(std::string in_file)
+  /// Constructor initializes everything
+  Simulation(Input &in, IO &out, RNG &rng, const uint32_t proc_i)
+    : path(proc_i, in, out, rng)
   {
-    // Input
-    in.Load(in_file);
+    // Initialize Actions
+    out.CreateGroup("Actions");
+    for (auto& input: in.GetChild("Actions").GetChildList("Action"))
+      actions.push_back(ActionFactory(input,out,path));
 
-    // Build MPI Model
-    uint32_t procs_per_group = in.GetChild("Parallel").GetAttribute<uint32_t>("procs_per_group",1);
-    uint32_t N = world_comm.NumProcs();
-    assert ((N % procs_per_group) == 0);
-    uint32_t n_groups = N/procs_per_group;
-    uint32_t my_group = world_comm.MyProc()/procs_per_group;
-    world_comm.Split(my_group, intra_comm);
-    vec<int> ranks(n_groups);
-    for (uint32_t group=0; group<n_groups; group++)
-      ranks(group) = group*procs_per_group;
-    world_comm.Subset(ranks, inter_comm);
-    int n_threads = 1;
-#if USE_OPENMP
-    #pragma omp parallel
-    {
-      n_threads = omp_get_num_threads();
-    }
-#endif
-    if (world_comm.MyProc() == 0) {
-      std::cout << "Running simpimc on " << in_file << std::endl;
-      std::cout <<"# Processes: "<< N << ", # Groups: "<< n_groups
-           <<", # Processes/Group: "<< procs_per_group
-           <<", # Threads/Process: "<< n_threads << std::endl;
-    }
+    // Initialize Moves
+    out.CreateGroup("Moves");
+    for (auto& input: in.GetChild("Moves").GetChildList("Move"))
+      events.push_back(MoveFactory(input,out,path,rng,actions));
 
-    // Output
-    std::stringstream tmp_ss;
-    tmp_ss << in.GetChild("IO").GetAttribute<std::string>("output_prefix") << "." << my_group << ".h5";
-    std::string output = tmp_ss.str();
-    out.Load(output);
-    out.Create();
+    // Initialize Observables
+    out.CreateGroup("Observables");
+    for (auto& input: in.GetChild("Observables").GetChildList("Observable"))
+      events.push_back(ObservableFactory(input,out,path,actions,events));
 
-    // Write input data
-    out.CreateGroup("Input");
-    out.Write("Input/file_name",in_file);
-    std::string in_string = in.GetString();
-    out.Write("Input/contents",in_string);
+    // Initialize Write
+    events.push_back(std::make_shared<Writes>(out,events,actions,proc_i));
+
+    // Initialize Algorithm
+    Input algorithm_input = in.GetChild("Algorithm");
+    main_loop.ReadLoop(algorithm_input,events);
   }
 
-  /// Runs the simulation.
-  void Run()
-  {
-    Input rng_in = in.GetChild("RNG");
-    int seed = 12345;
-    if (rng_in.node.name == "null") {
-#if USE_MPI
-      seed = (int)time(0)*(world_comm.MyProc()+1);
-#else
-      seed = (int)time(0);
-#endif
-    } else {
-#if USE_MPI
-      seed = in.GetChild("RNG").GetAttribute<int>("seed",(int)time(0)*(world_comm.MyProc()+1));
-#else
-      seed = in.GetChild("RNG").GetAttribute<int>("seed",(int)time(0));
-#endif
-    }
-    RNG rng(seed);
-    out.CreateGroup("RNG");
-    out.Write("RNG/seed",seed);
-
-    // Algorithm
-    Algorithm alg(in, out, rng, inter_comm.MyProc());
-    alg.Run();
-  }
+  /// Runs the algorithm by calling the DoEvent of the main_loop
+  void Run() { main_loop.DoEvent(); };
 };
 
 #endif // SIMPIMC_SIMULATION_CLASS_H_
